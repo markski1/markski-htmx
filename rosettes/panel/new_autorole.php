c760e405f285e67dc8d471158f762b98862b2579
Juan G <me@markski.ar>
2024-01-02 19:37:27 -0300

    move settings and creations page to the template thing

--- rosettes/panel/new_autorole.php
+++ rosettes/panel/new_autorole.php
@@ -1,197 +1,195 @@
 <?php
-    // Due to it's complexity this page is yet to be moved to the new templating thing.
-	include_once("funcs.php");
-	$db = db_connect();
+// Due to it's complexity this page is yet to be moved to the new templating thing.
+include_once("funcs.php");
+$db = db_connect();
 
-	if (!CheckSession($db, $namecache, $id)) {
-		Header('Location: index');
-		exit;
-	}
+if (!CheckSession($db, $namecache, $id)) {
+    Header('Location: index');
+    exit;
+}
 
-	if (!isset($_GET['guild'])) {
-		Header('Location: panel');
-		exit;
-	}
+if (!isset($_GET['guild'])) {
+    Header('Location: panel');
+    exit;
+}
 
-	if (!ctype_alnum($_GET['guild'])) {
-		Header('Location: panel');
-		exit;
-	}
+if (!ctype_alnum($_GET['guild'])) {
+    Header('Location: panel');
+    exit;
+}
 
-	$GuildId = $_GET['guild'];
+$GuildId = $_GET['guild'];
 
-    $guild_info = GetGuildInfo($db, $GuildId);
+$guild_info = GetGuildInfo($db, $GuildId);
 
-	$query = $db->prepare("SELECT * FROM roles WHERE guildid = ?");
-    $query->bind_param("s", $GuildId);
-    $query->execute();
-	$result = $query->get_result();
-	$roles = $result->fetch_all(MYSQLI_ASSOC);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Rosettes - Role Management</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<div class="container">
-		<center>
-			<h1 class="title">Rosettes</h1>
-			<p class="headingMd">Creating new AutoRole for: <?=$guild_info['namecache']?></p>
-			<hr />
-			<p class="headingMd"><b>AutoRole Maker</b></p>
-			<p>Select emoji and role, then add.</p>
-			<button type="button" id="select-emoji" class='button' style='width: 10rem;margin: 10px'>Select Emoji</button>
-			<select onchange="selectRole()" class="input" id="role_selector">
-				<option selected value="0">Select Role</option>
-				<?php
-					foreach ($roles as $role) {
-						$color = $role["color"];
-						// color-less roles default to #000000, this is unreadable
-						if ($color == "#000000") {
-							$color = "white";
-						}
-						echo '<option value="'.$role["id"].'" style="background-color:'.$color.'">'.$role["rolename"].'</option>';
-					}
-				?>
-			</select>
-			<p>When user clicks <span id="emoji_preview">__</span> they get "<b><span id="role_preview">______</span>"</b>
-			<button type="button" class='button' style='width: 5rem;margin: 10px' onclick="addSelection()">Add</button></p>
-			<div id="RoleEmojiListContainer">
-				<hr style="margin-top: 30px;"/>
-				<p><big>AutoRole entries:</big><p>
-				<span id="RoleEmojiList">
+$query = $db->prepare("SELECT * FROM roles WHERE guildid = ?");
+$query->bind_param("s", $GuildId);
+$query->execute();
+$result = $query->get_result();
+$roles = $result->fetch_all(MYSQLI_ASSOC);
 
-				</span>
-				<p><small>You have <span id="SlotLeftCount">15</span> slots left.</small></p>
-			</div>
-			<form method="POST" action="create_autoroles.php" id="ConfirmAutoRoles" style="display: none" onsubmit="return CheckRoleForm()">
-				<hr />
-				<input type="text" hidden value="<?=$GuildId?>" name="guild"/>
-				<div id="DynamicForm" style="display: none">
-				</div>
-				<p>Enter a name<br/>
-				<small>Example: Colors, Notifications, Interests, etc.<br/>Only letters, numbers, exclamation/question and punctuation symbols allowed.</small></p>
-				<input type="text" class="input" name="name" /><br/>
-				<button type="submit" class='button' style='width: 14rem; margin-top: 10px'>Create new AutoRole</button>
-			</form>
-			<a href="roles?guild=<?=$GuildId?>"><button type="button" class='button' style='width: 10rem; margin: 10px'>Return</button></a>
-		</center>
-	</div>
-</body>
-</html>
-<script type="text/javascript" src="js/fgEmojiPicker.js"></script>
-<script type="text/javascript">
-
-new FgEmojiPicker({
-	trigger: ['#select-emoji'],
-	postion: ['bottom'],
-    emit(obj, triggerElement) {
-    	const emoji = obj.emoji;
-    	selectEmoji(emoji);
+$role_list = "";
+foreach ($roles as $role) {
+    $color = $role["color"];
+    // color-less roles default to #000000, this is unreadable
+    if ($color == "#000000") {
+        $color = "white";
     }
-});
-
-
-var selectedEmoji = "__";
-var selectedRoleId = 0;
-var selectedRoleName = "______";
-
-const ListAddedRoles = [];
-
-const maxEntries = 15;
-var count = 0;
-
-const ListRoles = [
-	<?php
-		$last = array_pop($roles);
-		array_push($roles, $last);
-		foreach ($roles as $role) {
-			echo '{roleName:"'.$role["rolename"].'", roleId:"'.$role["id"].'"}';
-			if ($role != $last) echo ',';
-		}
-	?>
-];
-
-function selectEmoji(emoji) {
-	document.getElementById("emoji_preview").innerHTML = emoji;
-	selectedEmoji = emoji;
+    $role_list .= '<option value="'.$role["id"].'" style="background-color:'.$color.'">'.$role["rolename"].'</option>';
 }
 
-function selectRole() {
-	var checkRole = document.getElementById("role_selector").value;
-	if (checkRole == 0) return;
-
-	ListRoles.forEach(setSelectedRole)
+$script_role_list = "";
+$last = array_pop($roles);
+array_push($roles, $last);
+foreach ($roles as $role) {
+    $script_role_list .= '{roleName:"'.$role["rolename"].'", roleId:"'.$role["id"].'"}';
+    if ($role != $last) $script_role_list .= ',';
 }
 
-function setSelectedRole(item, index) {
-	var checkRole = document.getElementById("role_selector").value;
-	if (checkRole == item.roleId) {
-		selectedRoleId = item.roleId;
-		selectedRoleName = item.roleName;
-		document.getElementById("role_preview").innerHTML = item.roleName;
-	}
-}
+$site = new PanelTemplate("Role Management");
 
-function addSelection() {
-	if (selectedEmoji == "__") {
-		alert("You must select a valid emoji.");
-		return 0;
-	}
-	if (selectedRoleId == 0) {
-		alert("You must select a valid role.");
-		return 0;
-	}
-	if (count == maxEntries) {
-		alert("Sorry, you cannot add more than " + maxEntries + " role selections to AutoRoles.");
-		return 0;
-	}
-	ListAddedRoles.push({roleName: selectedRoleName, roleId: selectedRoleId, roleEmoji: selectedEmoji});
-	selectedEmoji = "__";
-	selectedRoleId = 0;
-	selectedRoleName = "______";
-	document.getElementById("role_preview").innerHTML = selectedRoleName;
-	document.getElementById("emoji_preview").innerHTML = selectedEmoji;
-	document.getElementById("role_selector").value = 0;
-	document.getElementById("RoleEmojiList").innerHTML = "";
-	document.getElementById("ConfirmAutoRoles").style.display = "block";
-	document.getElementById("DynamicForm").innerHTML = "";
+$content = /** @lang HTML */
+    <<<EOD
 
-	count++;
-	document.getElementById("SlotLeftCount").innerHTML = maxEntries - count;
 
-	ListAddedRoles.forEach(printRoleEntry);
-}
+    <h1 class="title">Rosettes</h1>
+    <p class="headingMd">Creating new AutoRole for: {$guild_info['namecache']}</p>
+    <hr />
+    <p class="headingMd"><b>AutoRole Maker</b></p>
+    <p>Select emoji and role, then add.</p>
+    <button type="button" id="select-emoji" class='button' style='width: 10rem;margin: 10px'>Select Emoji</button>
+    <select onchange="selectRole()" class="input" id="role_selector">
+        <option selected value="0">Select Role</option>
+        {$role_list}
+    </select>
+    <p>When user clicks <span id="emoji_preview">__</span> they get "<b><span id="role_preview">______</span>"</b>
+    <button type="button" class='button' style='width: 5rem;margin: 10px' onclick="addSelection()">Add</button></p>
+    <div id="RoleEmojiListContainer">
+        <hr style="margin-top: 30px;"/>
+        <p><big>AutoRole entries:</big><p>
+        <span id="RoleEmojiList">
 
-function printRoleEntry(item, index) {
-	document.getElementById("RoleEmojiList").innerHTML += "<p>When user clicks " + item.roleEmoji + " they get \"<b>" + item.roleName + "</b>\".</p>\n";
-	var form1 = document.createElement("input");
-	form1.type = "text";
-	form1.name = "roleEntry[" + index + "][emoji]";
-	form1.value = item.roleEmoji;
-	var form2 = document.createElement("input");
-	form2.type = "text";
-	form2.name = "roleEntry[" + index + "][roleid]";
-	form2.value = item.roleId;
-	document.getElementById("DynamicForm").appendChild(form1);
-	document.getElementById("DynamicForm").appendChild(form2);
-}
+        </span>
+        <p><small>You have <span id="SlotLeftCount">15</span> slots left.</small></p>
+    </div>
+    <form method="POST" action="create_autoroles.php" id="ConfirmAutoRoles" style="display: none" onsubmit="return CheckRoleForm()">
+        <hr />
+        <input type="text" hidden value="{$GuildId}" name="guild"/>
+        <div id="DynamicForm" style="display: none">
+        </div>
+        <p>Enter a name<br/>
+        <small>Example: Colors, Notifications, Interests, etc.<br/>Only letters, numbers, exclamation/question and punctuation symbols allowed.</small></p>
+        <input type="text" class="input" name="name" /><br/>
+        <button type="submit" class='button' style='width: 14rem; margin-top: 10px'>Create new AutoRole</button>
+    </form>
+    <a href="roles?guild={$GuildId}"><button type="button" class='button' style='width: 10rem; margin: 10px'>Return</button></a>
+	
+    <script type="text/javascript" src="js/fgEmojiPicker.js"></script>
+    <script type="text/javascript">
+        
+        new FgEmojiPicker({
+            trigger: ['#select-emoji'],
+            postion: ['bottom'],
+            emit(obj, triggerElement) {
+                const emoji = obj.emoji;
+                selectEmoji(emoji);
+            }
+        });
+        
+        
+        var selectedEmoji = "__";
+        var selectedRoleId = 0;
+        var selectedRoleName = "______";
+        
+        const ListAddedRoles = [];
+        
+        const maxEntries = 15;
+        var count = 0;
+        
+        const ListRoles = [
+            {$script_role_list}
+        ];
+        
+        function selectEmoji(emoji) {
+            document.getElementById("emoji_preview").innerHTML = emoji;
+            selectedEmoji = emoji;
+        }
+        
+        function selectRole() {
+            var checkRole = document.getElementById("role_selector").value;
+            if (checkRole == 0) return;
+        
+            ListRoles.forEach(setSelectedRole)
+        }
+        
+        function setSelectedRole(item, index) {
+            var checkRole = document.getElementById("role_selector").value;
+            if (checkRole == item.roleId) {
+                selectedRoleId = item.roleId;
+                selectedRoleName = item.roleName;
+                document.getElementById("role_preview").innerHTML = item.roleName;
+            }
+        }
+        
+        function addSelection() {
+            if (selectedEmoji == "__") {
+                alert("You must select a valid emoji.");
+                return 0;
+            }
+            if (selectedRoleId == 0) {
+                alert("You must select a valid role.");
+                return 0;
+            }
+            if (count == maxEntries) {
+                alert("Sorry, you cannot add more than " + maxEntries + " role selections to AutoRoles.");
+                return 0;
+            }
+            ListAddedRoles.push({roleName: selectedRoleName, roleId: selectedRoleId, roleEmoji: selectedEmoji});
+            selectedEmoji = "__";
+            selectedRoleId = 0;
+            selectedRoleName = "______";
+            document.getElementById("role_preview").innerHTML = selectedRoleName;
+            document.getElementById("emoji_preview").innerHTML = selectedEmoji;
+            document.getElementById("role_selector").value = 0;
+            document.getElementById("RoleEmojiList").innerHTML = "";
+            document.getElementById("ConfirmAutoRoles").style.display = "block";
+            document.getElementById("DynamicForm").innerHTML = "";
+        
+            count++;
+            document.getElementById("SlotLeftCount").innerHTML = maxEntries - count;
+        
+            ListAddedRoles.forEach(printRoleEntry);
+        }
+        
+        function printRoleEntry(item, index) {
+            document.getElementById("RoleEmojiList").innerHTML += "<p>When user clicks " + item.roleEmoji + " they get \"<b>" + item.roleName + "</b>\".</p>\n";
+            var form1 = document.createElement("input");
+            form1.type = "text";
+            form1.name = "roleEntry[" + index + "][emoji]";
+            form1.value = item.roleEmoji;
+            var form2 = document.createElement("input");
+            form2.type = "text";
+            form2.name = "roleEntry[" + index + "][roleid]";
+            form2.value = item.roleId;
+            document.getElementById("DynamicForm").appendChild(form1);
+            document.getElementById("DynamicForm").appendChild(form2);
+        }
+        
+        function CheckRoleForm() {
+            if (count == 0) {
+                alert("You've not added any roles!");
+                return false;
+            }
+            if (count > maxEntries) {
+                alert("SUS! You've more roles than the panel should allow!!!");
+                return false;
+            }
+            return true;
+        }
+        
+    </script>
 
-function CheckRoleForm() {
-	if (count == 0) {
-		alert("You've not added any roles!");
-		return false;
-	}
-	if (count > maxEntries) {
-		alert("SUS! You've more roles than the panel should allow!!!");
-		return false;
-	}
-	return true;
-}
 
-</script>
+EOD;
+
+$site->render($content);
--- rosettes/panel/settings.php
+++ rosettes/panel/settings.php
@@ -1,101 +1,93 @@
 <?php
-    // Yet-unupgraded due to complexity
-	include_once("funcs.php");
-	$db = db_connect();
+include_once("funcs.php");
+$db = db_connect();
 
-	if (!CheckSession($db, $namecache, $id)) {
-		Header('Location: index');
-		exit;
-	}
+if (!CheckSession($db, $namecache, $id)) {
+    Header('Location: index');
+    exit;
+}
 
-	if (!isset($_GET['guild'])) {
-		Header('Location: panel');
-		exit;
-	}
+if (!isset($_GET['guild'])) {
+    Header('Location: panel');
+    exit;
+}
 
-	if (!ctype_alnum($_GET['guild'])) {
-		Header('Location: panel');
-		exit;
-	}
+if (!ctype_alnum($_GET['guild'])) {
+    Header('Location: panel');
+    exit;
+}
 
-	$GuildId = $_GET['guild'];
+$GuildId = $_GET['guild'];
+
+// Obtain the guild's settings
+
+$guild_info = GetGuildInfo($db, $GuildId);
+
+$message_processing = $guild_info['settings'][0];
+$random_commands = $guild_info['settings'][2];
+$vcmon = $guild_info['settings'][5];
+$minigame_commands = $guild_info['settings'][4];
+
+$select_message_enabled = ($message_processing >= 1) ? 'selected' : '';
+$select_random_enabled = ($random_commands >= 1) ? 'selected' : '';
+$select_minigame_enabled = ($minigame_commands >= 1) ? 'selected' : '';
+$select_vcmon_enabled = ($vcmon >= 1) ? 'selected' : '';
+
+if (isset($_GET['success'])) {
+    $message = "<p style='color: lightgreen'>Changes applied.</p>";
+}
+else if (isset($_GET['error'])) {
+    $message = "<p style='color: lightred'>Something went wrong.</p>";
+}
+else {
+    $message = '';
+}
+
+$site = new PanelTemplate("Settings");
+
+$content = /** @lang HTML */
+<<<EOD
+    
+    
+    <h1 class="title">Rosettes</h1>
+    <p class="headingMd">Guild: {$guild_info['namecache']}</p>
+    <hr />
+    <p class="headingMd"><b>Settings</b></p>
+    <form method="POST" action="update_guild.php">
+        <div class='headingContainer'>
+            <p>Message parsing*<br/>
+            <select class="input" name="message_processing">
+                <option value="0">Disabled</option>
+                <option {$select_message_enabled} value="1">Enabled</option>
+            </select></p>
+            <p>Allow random commands<br/>
+            <select class="input" name="random_commands">
+                <option value="0">Disabled</option>
+                <option {$select_random_enabled} value="1">Enabled</option>
+            </select></p>
+            <p>Allow Farming/Fishing commands<br/>
+            <select class="input" name="minigame_commands">
+                <option value="0">Disabled</option>
+                <option {$select_minigame_enabled} value="1">Enabled</option>
+            </select></p>
+            <p>Monitor for voicechat joins and leaves<br/>
+            <select class="input" name="vc_monitor">
+                <option value="0">Disabled</option>
+                <option {$select_vcmon_enabled} value="1">Enabled</option>
+            </select></p>
+        </div>
+        <input type="text" hidden value="{$GuildId}" name="guild"/>
+        <input type="submit" class="button" style="width: 10rem" value="Update settings" />
+        <a href="panel"><button type="button" class='button' style='width: 12rem; margin: 10px'>Return</button></a>
+        {$message}
+    </form>
+    <p>Danger zone</p>
+    <a href="nuke?guild={$GuildId}"><button class='button' style='width: 12rem; margin: 10px'>Forget this guild</button></a>
+    <p><small>* Message parsing means wether or not messages will be scanned for stuff like Steam or expiring links, to which Rosettes has replies to.<br/>
+        In any case, messages are never logged, and their content is trashed after parsing.
+    </small></p>
 
 
+EOD;
 
-	// Obtain the guild's settings
-
-    $guild_info = GetGuildInfo($db, $GuildId);
-
-	$message_processing = $guild_info['settings'][0];
-	$music_commands = $guild_info['settings'][1];
-	$random_commands = $guild_info['settings'][2];
-	$vc_monitor = $guild_info['settings'][5];
-	$farm_commands = $guild_info['settings'][4];
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Rosettes - Settings</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<div class="container">
-		<center>
-			<h1 class="title">Rosettes</h1>
-			<p class="headingMd">Guild: <?=$guild_info['namecache']?></p>
-			<hr />
-			<p class="headingMd"><b>Settings</b></p>
-			<form method="POST" action="update_guild.php">
-				<div class='headingContainer'>
-					<p>Message parsing*<br/>
-					<select class="input" name="message_processing">
-						<option <?php if ($message_processing == 0) echo 'selected'; ?> value="0">Disabled</option>
-						<option <?php if ($message_processing >= 1) echo 'selected'; ?> value="1">Enabled</option>
-					</select></p>
-					<p>Allow music commands<br/>
-					<select class="input" name="music_commands">
-						<option <?php if ($music_commands == 0) echo 'selected'; ?> value="0">Disabled</option>
-						<option <?php if ($music_commands == 1) echo 'selected'; ?> value="1">Enabled</option>
-					</select></p>
-					<p>Allow random commands<br/>
-					<select class="input" name="random_commands">
-						<option <?php if ($random_commands == 0) echo 'selected'; ?> value="0">Disabled</option>
-						<option <?php if ($random_commands == 1) echo 'selected'; ?> value="1">Enabled</option>
-					</select></p>
-					<p>Allow Farming/Fishing commands<br/>
-					<select class="input" name="farm_commands">
-						<option <?php if ($farm_commands == 0) echo 'selected'; ?> value="0">Disabled</option>
-						<option <?php if ($farm_commands == 1) echo 'selected'; ?> value="1">Enabled</option>
-					</select></p>
-					<p>Monitor for voicechat joins and leaves<br/>
-					<select class="input" name="vc_monitor">
-						<option <?php if ($vc_monitor == 0) echo 'selected'; ?> value="0">Disabled</option>
-						<option <?php if ($vc_monitor == 1) echo 'selected'; ?> value="1">Enabled</option>
-					</select></p>
-				</div>
-				<input type="text" hidden value="<?=$GuildId?>" name="guild"/>
-				<input type="submit" class="button" style="width: 10rem" value="Update settings" />
-				<a href="panel"><button type="button" class='button' style='width: 12rem; margin: 10px'>Return</button></a>
-				<?php
-					if (isset($_GET['success'])) {
-						echo "<p style='color: lightgreen'>Changes applied.</p>";
-					}
-					else if (isset($_GET['error'])) {
-						echo "<p style='color: lightred'>Something went wrong.</p>";
-					}
-				?>
-			</form>
-			<p>Danger zone</p>
-			<a href="nuke?guild=<?=$GuildId?>"><button class='button' style='width: 12rem; margin: 10px'>Forget this guild</button></a>
-		</center>
-			<p><small>* Message parsing means wether or not messages will be scanned for stuff like Steam or expiring links, to which Rosettes has replies to.<br/>
-				In any case, messages are never logged, and their content is trashed after parsing.
-			</small></p>
-			
-	</div>
-</body>
-</html>
+$site->render($content);
