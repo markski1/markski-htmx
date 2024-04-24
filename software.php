452ab4a3703c2cda28d391d836d9f20c03d78cbf
Juan G. <me@markski.ar>
2024-04-23 23:56:42 -0300

    update wttk screenshot

--- software.php
+++ software.php
@@ -25,7 +25,7 @@ $content = /** @lang HTML */
     <div>
         <h4>WTTk</h4>
         enables tweaking hidden windows settings.<br />
-        <sitelink to="wttk">learn more</sitelink> - <a href="https://github.com/markski1/WTTk" target="_blank">github</a>
+        <sitelink to="wttk">learn more</sitelink> - <a href="https://github.com/markski1/WinTweakTool" target="_blank">github</a>
     </div>
     <div>
         <h4>BSXU</h4>
